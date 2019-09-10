d2750aa09ef44ad24525b21f9ce21565b10d593f
jessicp0249 <42855762+jessicp0249@users.noreply.github.com>
2019-09-10 15:53:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,19 @@
 # Email: jessicp0249@student.vvc.edu
 # Date created: 10 Sep 2019
 
+# Tax rates
 state_tax_rate = 0.05
 sales_tax_rate = 0.025
 
+# Get subtotal from user
 subtotal = float(input('Please enter your purchase subtotal:\n'))
+
+# Calculate taxes and final total
 state_tax = state_tax_rate * subtotal
 sales_tax = sales_tax_rate * subtotal
 total = subtotal + state_tax + sales_tax
 
+# Print receipt
 print('Subtotal: $%0.2f' % (subtotal))
 print('State tax: $%0.4f' % (state_tax))
 print('County sales tax: $%0.4f' % (sales_tax))

dc5b40ed91ead3d38ee0acd9b908c9b54d83ff21
jessicp0249 <42855762+jessicp0249@users.noreply.github.com>
2019-09-10 15:52:20 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# File: main.py
+# Description: Program will prompt user for a purchase subtotal, calculate tax, 
+# and print a receipt with the subtotal, taxes, and total.
+# Author: Jessica Priester
+# Email: jessicp0249@student.vvc.edu
+# Date created: 10 Sep 2019
+
+state_tax_rate = 0.05
+sales_tax_rate = 0.025
+
+subtotal = float(input('Please enter your purchase subtotal:\n'))
+state_tax = state_tax_rate * subtotal
+sales_tax = sales_tax_rate * subtotal
+total = subtotal + state_tax + sales_tax
+
+print('Subtotal: $%0.2f' % (subtotal))
+print('State tax: $%0.4f' % (state_tax))
+print('County sales tax: $%0.4f' % (sales_tax))
+print('Total tax: $%0.4f' % (state_tax + sales_tax))
+print('Total cost: $%0.2f' % (total))
